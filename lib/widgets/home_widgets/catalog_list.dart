345264894dab6b62c69a36bda5cb0ec9b0c6d26e
syedimaduddin <86525904+syedimaduddin@users.noreply.github.com>
2021-02-07 05:30:00 +0530

    The catalog app is near to complete, only url_strategy is left for the catalog app

--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -1,7 +1,6 @@
-import 'package:first_flutter_app/models/cart.dart';
 import 'package:first_flutter_app/models/catalog.dart';
 import 'package:first_flutter_app/pages/home_detail_page.dart';
-import 'package:first_flutter_app/widgets/themes.dart';
+import 'package:first_flutter_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'add_to_cart.dart';
@@ -10,7 +9,9 @@ import 'catalog_image.dart';
 class CatalogList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return !context.isMobile ?
+    GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, crossAxisSpacing: 20.0),
       shrinkWrap: true,
       itemCount: CatalogModel.items.length,
       itemBuilder: (context, index) {
@@ -23,6 +24,26 @@ class CatalogList extends StatelessWidget {
                   ),
                 ),
             child: CatalogItem(catalog: catalog));
+      },
+    ) : ListView.builder(
+      shrinkWrap: true,
+      itemCount: CatalogModel.items.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogModel.items[index];
+        return InkWell(
+            onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeDetailPage(catalog: catalog),
+                  ),
+                ),
+            // onTap: () => context.vxNav.push(Uri(
+            //   path: MyRoutes.homeDetailsRoute, 
+            //   queryParameters: {"id": catalog.id}), 
+            //   params: catalog,
+            //   ),
+            child: CatalogItem(catalog: catalog),
+        );
       },
     );
   }
@@ -37,9 +58,7 @@ class CatalogItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return VxBox(
-      child: Row(
-        children: [
+    var children2 = [
           Hero(
             tag: Key(catalog.id.toString()),
             child: CatalogImage(
@@ -64,10 +83,15 @@ class CatalogItem extends StatelessWidget {
                   ],
                 ).pOnly(right: 8.0),
               ],
-            ),
+            ).p(context.isMobile ? 0 : 16),
           ),
-        ],
-      ),
+        ];
+    return VxBox(
+      child: context.isMobile? Row(
+        children: children2,
+      ) : Column(
+        children: children2,
+        ),
     ).color(context.cardColor).rounded.square(150).make().py16();
   }
 }
