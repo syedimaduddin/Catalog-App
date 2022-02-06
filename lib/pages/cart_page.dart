4a4db7ef74658081a392a46a511339c1b59b76cf
syedimaduddin <86525904+syedimaduddin@users.noreply.github.com>
2022-02-06 16:48:59 +0530

    Add a new feature of Dark Theme to my catalog app which can interchange with the light theme from the system

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -6,10 +6,10 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MyTheme.creamColor,
+      backgroundColor: context.canvasColor,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        title: "Cart".text.color(MyTheme.darkBluishColor).make(),
+        title: "Cart".text.color(context.theme.buttonColor).make(),
       ),
     );
   }
--- lib/widgets/home_widgets/catalog_header.dart
+++ lib/widgets/home_widgets/catalog_header.dart
@@ -8,7 +8,7 @@ class CatalogHeader extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        "Catalog App".text.xl4.bold.color(MyTheme.darkBluishColor).make(),
+        "Catalog App".text.xl4.bold.color(context.theme.accentColor).make(),
         "Trending Products".text.lg.make(),
       ],
     );
