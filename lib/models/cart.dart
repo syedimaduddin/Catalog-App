84b0c156ddd180b22e8c6689ab27c94108160b04
syedimaduddin <86525904+syedimaduddin@users.noreply.github.com>
2022-02-11 11:22:54 +0530

    Created a store to add and remove items from cart

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,12 +1,8 @@
+import 'package:first_flutter_app/core/store.dart';
 import 'package:first_flutter_app/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
-  static final cartModel = CartModel._internal();
-
-  CartModel._internal();
-
-  factory CartModel() => cartModel;
-
   // Catalog Field
   late CatalogModel _catalog;
 
@@ -29,12 +25,22 @@ class CartModel {
       items.fold(0, (total, current) => total + current.price);
 
   // Add item
-  void add(Item item) {
-    _itemIds.add(item.id);
-  }
+ 
 
   // Remove item
   void remove(Item item) {
     _itemIds.remove(item.id);
   }
 }
+
+class AddMutation extends VxMutation<MyStore> {
+  final Item item;
+
+  AddMutation(this.item);
+
+  @override
+  perform() {
+    store?.cart._itemIds.add(item.id);
+  }
+}
+
